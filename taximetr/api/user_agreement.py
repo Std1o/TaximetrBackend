5b2adc1a57d4207ef807741e305a930cf64026c1
Std1o <37378410+Std1o@users.noreply.github.com>
2026-05-14 19:35:56 +0300

    Update user_agreement.py

--- taximetr/api/user_agreement.py
+++ taximetr/api/user_agreement.py
@@ -13,16 +13,21 @@ router = APIRouter(prefix='/user_agreement', tags=["user_agreement"])
 def get_file(file_name: str):
     return FileResponse(f"taximetr/files/{file_name}".encode('utf-8').decode('utf-8'))
 
+
 @router.post("/upload/")
 async def create_upload_file(request: Request, upload_file: UploadFile):
     try:
+        # Абсолютный путь или относительный - не важно, главное создать
+        os.makedirs("taximetr/files", exist_ok=True)
+
         file_path = f"taximetr/files/{upload_file.filename}"
         with open(file_path, "wb") as buffer:
             shutil.copyfileobj(upload_file.file, buffer)
     finally:
         upload_file.file.close()
+
     base_url = str(request.base_url).rstrip('/')
-    return PlainTextResponse(f"{base_url}/files/{upload_file.filename}")
+    return PlainTextResponse(f"{base_url}/user_agreement/{upload_file.filename}")
 
 @router.get("/")
 def get_user_agreement(service: UserAgreementService = Depends()):
