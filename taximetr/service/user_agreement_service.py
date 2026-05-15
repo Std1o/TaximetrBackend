e9b0fa7db386676237ab9d172b47dfa171426258
Std1o <37378410+Std1o@users.noreply.github.com>
2026-05-15 12:20:56 +0300

    upd create_user_agreement

--- taximetr/service/user_agreement_service.py
+++ taximetr/service/user_agreement_service.py
@@ -9,12 +9,16 @@ class UserAgreementService:
         self.session = session
 
     def get_user_agreement(self) -> tables.UserAgreement:
-        user_agreement = self.session.query(tables.UserAgreement).first()
-        return user_agreement
+        user_agreement: tables.UserAgreement = self.session.query(tables.UserAgreement).first()
+        return user_agreement.file_url
 
     def create_user_agreement(self, file_url: str) -> tables.UserAgreement:
-        user_agreement = tables.UserAgreement(file_url=file_url)
-        self.session.add(user_agreement)
+        user_agreement = self.get_user_agreement()
+        if user_agreement:
+            user_agreement.file_url = file_url
+        if not user_agreement:
+            user_agreement = tables.UserAgreement(file_url=file_url)
+            self.session.add(user_agreement)
         self.session.commit()
         self.session.refresh(user_agreement)
         return user_agreement
